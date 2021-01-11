69eb5e8311c6b6973c28eff4c11d536d5ea3f1fb
Michael <michaelbubel.1989@gmail.com>
2021-01-10 17:21:58 -0800

    Use connection pooling

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,12 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
   host: process.env.DATABASE_HOST || "localhost",
   port: 3306,
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "blinkM123",
-  database: process.env.DATABASE || "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+  database: process.env.DATABASE || "burgers_db",
 });
 
 // Export connection for our ORM to use.
