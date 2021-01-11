296eedf7fcddf12e3ab632410bb65aa301a9bf72
Michael <michaelbubel.1989@gmail.com>
2021-01-10 17:31:32 -0800

    removed index from get

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/index", (req, res) => {
+router.get("/", (req, res) => {
   burger.findAll((data) => {
     let hbsObject = {
       burgers: data,
